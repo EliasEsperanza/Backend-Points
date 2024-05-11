35e4838ba553105a31836a3380001b504601dd37
Elias Esperanza <eliasesperanza261380@gmail.com>
2024-05-10 18:09:51 -0600

    Arreglo de conflictos

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import clienteRoutes from './Cliente/cliente.routes.js';
 import categoriaClienteRoutes from './CategoriaCliente/categoriaCliente.routes.js';
 import tipoClienteRoutes from './TipoCliente/tipoCliente.routes.js';
 import  nivelesRoutes  from "./Niveles/niveles.routes.js";
+import canjeRoutes from "./Canje/canje.routes.js";
 import usuarioRoutes from './Usuario/usuario.routes.js';
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(clienteRoutes);
 app.use(categoriaClienteRoutes);
 app.use(tipoClienteRoutes);
 app.use(nivelesRoutes);
+app.use(canjeRoutes)
 app.use(usuarioRoutes);
 
 export default app;
