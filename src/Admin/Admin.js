d89d27cd693002a75280c624cbfb63881921bbfc
Elias Esperanza <eliasesperanza261380@gmail.com>
2024-05-13 19:57:30 -0600

    Login funcional para cliente y vendedores

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,6 +1,5 @@
 import { sequelize } from '../database/database.js';
 import { DataTypes } from 'sequelize';
-import { Cliente } from "../Cliente/Cliente.js";
 
 export const Admin = sequelize.define('tbl_usuario_administrador',{
     idAdmin:{
@@ -16,13 +15,7 @@ export const Admin = sequelize.define('tbl_usuario_administrador',{
         type:DataTypes.STRING(300),
         allowNull:false
     },
-    idCliente:{
-        type: DataTypes.INTEGER,
-        allowNull:false
-    }
 },{
     timestamps: false,
     tableName: 'tbl_usuario_Administrador'
 });
-
-Admin.belongsTo(Cliente, { foreignKey: 'idCliente' });
--- src/auth/login.routes.js
+++ src/auth/login.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { login } from "./login.js";
+import { login, logout } from "./login.js";
 
 const router = Router();
 router.post('/login', login);
+router.post('/logout', logout);
 
 export default router;
