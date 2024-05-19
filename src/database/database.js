6bc26413fe64e6058a8c97d95ee3e9f8fee27ac5
EliasEsperanza <82784538+EliasEsperanza@users.noreply.github.com>
2024-05-18 21:08:40 -0600

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -11,8 +11,9 @@ const databaseUser = process.env.DB_USER;
 const databaseHost = process.env.DB_HOST;
 const databaseDialect = process.env.DB_DIALECT;
 const databasePort =  process.env.DB_PORT;
+const databasePassword = process.env.DB_PASS;
 
-export const sequelize = new Sequelize(databaseName, databaseUser, "", {
+export const sequelize = new Sequelize(databaseName, databaseUser, databasePassword, {
     host: databaseHost,
     port: databasePort,
     dialect: databaseDialect
